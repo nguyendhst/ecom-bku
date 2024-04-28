5b0e3925cbd3bbd8ead2be8847b6267f6fc791aa
Huy-Thai-3105 <80185943+Huy-Thai-3105@users.noreply.github.com>
2024-04-28 23:53:59 +0700

    add b2b page, autofill form

--- app/checkout/_components/customer-info-form.tsx
+++ app/checkout/_components/customer-info-form.tsx
@@ -14,10 +14,11 @@ import {
 	FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import React, { forwardRef, useImperativeHandle } from "react";
+import React, { forwardRef, useEffect, useImperativeHandle, useState } from "react";
 import FormWrapper from "./form-wrapper";
 import { Textarea } from "../../../components/ui/textarea";
 import { useCheckoutForm } from "../../../store/checkout";
+import { GamepadIcon } from "lucide-react";
 
 const formSchema = z.object({
 	name: z.string().min(1).max(255),
@@ -34,7 +35,25 @@ const formSchema = z.object({
 });
 
 export const CustomerInfoForm = forwardRef((props, ref) => {
+
 	const { formData: data, setCustomerData } = useCheckoutForm();
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("123");
+	const [phone, setPhone] = useState("");
+	const [province, setProvince] = useState("");
+	const [district, setDistrict] = useState("");
+	const [ward, setWard] = useState("");
+	const [street, setStreet] = useState("");
+	const [address, setAddress] = useState("");
+	const [showModal, setShowModal] = useState(false);
+
+
+	useEffect(() => {
+		if (email == "huythai31052002@gmail.com") {
+			setShowModal(true);
+			console.log("đù má đéo hiểu")
+		}
+	},[email])
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		defaultValues: data.customer,
@@ -58,7 +77,17 @@ export const CustomerInfoForm = forwardRef((props, ref) => {
 	const validationFailedCallback = (errors: any) => {
 		console.log(errors);
 	};
-
+	const handleAutoFill = () => {
+		setShowModal(false);
+		setName("Trương Huy Thái")
+		setWard("Phường 14")
+		setProvince("HCM")
+		setDistrict("Gò Vấp")
+		setAddress("261/2")
+		setPhone("0348273184")
+		setStreet("Lê Đức Thọ")
+	}
+	console.log("showModal",showModal)
 	return (
 		<FormWrapper title="Customer Information" description="Enter your details">
 			<Form {...form}>
@@ -76,7 +105,7 @@ export const CustomerInfoForm = forwardRef((props, ref) => {
 							<FormItem>
 								<FormLabel htmlFor={field.name}>Name</FormLabel>
 								<FormControl>
-									<Input {...field} />
+									<Input {...field}  value={name} onChange={(e) => setName(e.target.value)} />
 								</FormControl>
 								<FormDescription>Enter your full name</FormDescription>
 								<FormMessage />
@@ -91,7 +120,7 @@ export const CustomerInfoForm = forwardRef((props, ref) => {
 								<FormItem>
 									<FormLabel htmlFor={field.name}>Email</FormLabel>
 									<FormControl>
-										<Input {...field} />
+										<Input {...field} value={email} onChange={(e) => setEmail(e.target.value)} />
 									</FormControl>
 									<FormDescription>Enter your email address</FormDescription>
 									<FormMessage />
@@ -105,7 +134,7 @@ export const CustomerInfoForm = forwardRef((props, ref) => {
 								<FormItem>
 									<FormLabel htmlFor={field.name}>Phone</FormLabel>
 									<FormControl>
-										<Input {...field} />
+										<Input {...field}  value={phone} onChange={(e) => setPhone(e.target.value)} />
 									</FormControl>
 									<FormDescription>Enter your phone number</FormDescription>
 									<FormMessage />
@@ -119,7 +148,7 @@ export const CustomerInfoForm = forwardRef((props, ref) => {
 								<FormItem>
 									<FormLabel htmlFor={field.name}>Province</FormLabel>
 									<FormControl>
-										<Input {...field} />
+										<Input {...field}  value={province} onChange={(e) => setProvince(e.target.value)} />
 									</FormControl>
 									<FormDescription>Enter your province</FormDescription>
 									<FormMessage />
@@ -133,7 +162,7 @@ export const CustomerInfoForm = forwardRef((props, ref) => {
 								<FormItem>
 									<FormLabel htmlFor={field.name}>District</FormLabel>
 									<FormControl>
-										<Input {...field} />
+										<Input {...field}  value={district} onChange={(e) => setDistrict(e.target.value)} />
 									</FormControl>
 									<FormDescription>Enter your district</FormDescription>
 									<FormMessage />
@@ -147,7 +176,7 @@ export const CustomerInfoForm = forwardRef((props, ref) => {
 								<FormItem>
 									<FormLabel htmlFor={field.name}>Ward</FormLabel>
 									<FormControl>
-										<Input {...field} />
+										<Input {...field}  value={ward} onChange={(e) => setWard(e.target.value)} />
 									</FormControl>
 									<FormDescription>Enter your ward</FormDescription>
 									<FormMessage />
@@ -161,7 +190,7 @@ export const CustomerInfoForm = forwardRef((props, ref) => {
 								<FormItem>
 									<FormLabel htmlFor={field.name}>Street</FormLabel>
 									<FormControl>
-										<Input {...field} />
+										<Input {...field}  value={street} onChange={(e) => setStreet(e.target.value)} />
 									</FormControl>
 									<FormDescription>Enter your street</FormDescription>
 									<FormMessage />
@@ -175,7 +204,7 @@ export const CustomerInfoForm = forwardRef((props, ref) => {
 								<FormItem>
 									<FormLabel htmlFor={field.name}>Address</FormLabel>
 									<FormControl>
-										<Input {...field} />
+										<Input {...field}  value={address} onChange={(e) => setAddress(e.target.value)}  />
 									</FormControl>
 									<FormDescription>Enter your address</FormDescription>
 									<FormMessage />
@@ -202,11 +231,52 @@ export const CustomerInfoForm = forwardRef((props, ref) => {
 								</FormItem>
 							)}
 						/>
-					</div>
+				
+				</div>
 
 					{/*<Button type="submit">Submit</Button>*/}
 				</form>
+				{showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none " style={{width: "600px"}}>
+
+                {/*body*/}
+                <div className="relative p-6 flex-auto" >
+                  <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+						Autofill you information form?
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={handleAutoFill}
+                  >
+                    Autofill
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
 			</Form>
+			
+
 		</FormWrapper>
 	);
 });
