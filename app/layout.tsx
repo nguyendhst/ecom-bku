52ba41ab68b60454d5d541f0b4c7b720006a00b6
trantuananh <anhtran338@gmail.com>
2024-05-01 11:08:16 +0700

    add gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/Script";
 import "./globals.css";
 //import CartProvider from "./components/Providers";
 import Navbar from "./components/Navbar";
@@ -21,6 +22,18 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+            <head>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-9V9B1E28FF"></Script>
+                <Script id="google-analytics">
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-9V9B1E28FF');
+                    `}
+                </Script>
+            </head>
             <body className={inter.className}>
                 <ShoppingCartProvider>
                     <Navbar />
